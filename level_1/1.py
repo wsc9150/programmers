b72be8aff4d3147f9d77fa97982f22153a7f24ae
wsc9150 <wsc9150@gmail.com>
2026-07-24 23:57:43 +0900

    260724 | Add level1

---
+++ level_1/1.py
@@ -0,0 +1,12 @@
+# 두 정수 사이의 합
+def solution(a, b):
+    answer = 0
+    
+    value_list = list(range(min(a, b), max(a, b) + 1))
+    answer = sum(value_list)
+    
+    return answer
+
+print(solution(3, 5))
+print(solution(3, 3))
+print(solution(5, 3))
